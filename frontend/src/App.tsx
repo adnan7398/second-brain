4598e35a78d73590b757ff9c7931747d110c6475
adnan7398 <123adnansiddiqui@gmail.com>
2024-11-27 01:03:23 +0530

    created card compenent and stated add content bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,10 +7,15 @@ import { Card } from './components/card'
 function App() {
 
 
-  return ( <div>
-      <Button variant='primary' text="Add Content"  startIcon ={<PlusIcon/>}></Button>
-      <Button variant = 'secondry' text="Share Brain"  startIcon={<ShareIcon/>}></Button>
-      <Card></Card>
+  return ( <div className='p-4'>
+      <div className='flex justify-end gap-4'>
+          <Button variant='primary' text="Add Content"  startIcon ={<PlusIcon/>}></Button>
+          <Button variant = 'secondry' text="Share Brain"  startIcon={<ShareIcon/>}></Button>
+      </div>
+      <div className='flex gap-4'>
+        <Card type='twitter' link="https://x.com/CPMG_Odisha/status/1691491338649481216" tittle="First Tweet"></Card>
+        <Card type='youtube' link="https://www.youtube.com/watch?v=yS8k-bWtMWk" tittle="First vedio"></Card>
+      </div>
   </div>
   )
 }
--- frontend/src/components/card.tsx
+++ frontend/src/components/card.tsx
@@ -1,7 +1,36 @@
- export function Card(){
+import { ShareIcon } from "../icons/sharIcon";
+interface Cardprops{
+    tittle:string;
+    link:string;
+    type:"youtube" | "twitter"
+}
+ export function Card({tittle,link,type}:Cardprops){
     return <div>
-        <div className=" p-8 bg-white rounded-md   border-gray-200 border   max-w-96">
-        hiii there 
-    </div>
+        <div className=" p-8 bg-white rounded-md   border-gray-200 border max-w-72 min-h-48 ">
+            <div className="flex justify-between">
+                <div className="flex items-center text-md">
+                   <div className="text-gray-500 pr-2">  
+                   <ShareIcon/>
+                   </div>
+                    {tittle}
+                </div>
+                <div className="flex items-center"> 
+                    <div className="pr-2 text-gray-500">
+                        <a href={link} target="_blank">
+                                <ShareIcon/>
+                            </a>
+                    </div>
+                    <div className="text-gray-500">
+                        <ShareIcon/>
+                    </div>
+                </div>
+            </div>
+            <div className="pt-4">
+                {type==="youtube" && <iframe  className="w-full" src={link.replace("watch","embed").replace("?v=" ,"/")} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>}
+                {type==="twitter" && <blockquote className="twitter-tweet w-full">
+                        <a href = {link.replace("x.com","twitter.com")}></a> 
+                    </blockquote>}
+             </div>
+        </div>
     </div>
  }
