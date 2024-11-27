6be527495412773d641ec7577f1d3baa62e532f2
adnan7398 <123adnansiddiqui@gmail.com>
2024-11-27 13:34:19 +0530

    dashboard completed

--- frontend/src/components/sidebar.tsx
+++ frontend/src/components/sidebar.tsx
@@ -1,9 +1,20 @@
 import { TwitterIcon } from "../icons/twittericon";
 import { Sidebaritems } from "./sidebaritem";
+import { YoutubeIcon } from "../icons/youtubeicon";
+import { Logo } from "../icons/logo";
 export function Sidebar(){
-    return <div className="h-screen bg-white border-r w-72 fixed left-0 top-0">
-        <div className="pt-4">
+    return <div className="h-screen bg-white border-r w-72 fixed left-0 top-0 pl-6">
+            <div className="flex text-2xl pt-8 items-center ">
+                <div className="text-purple-600 pr-2"><Logo/> </div>
+                <div>
+                    Second Brain
+                </div>
+            </div>
+            
+        <div className="pt-8 pl-4">
             <Sidebaritems text ="twitter" icon = {<TwitterIcon/> }/>
+            <Sidebaritems text ="Youtube" icon = {<YoutubeIcon/> }/>
+            
         </div>
     </div>
 }
--- frontend/src/components/sidebaritem.tsx
+++ frontend/src/components/sidebaritem.tsx
@@ -4,9 +4,9 @@ export function Sidebaritems({text,icon}:{
     text:string,
     icon:ReactElement
 }){
-    return <div className="flex">
-        <div className="p-2">{icon}</div>
-        <div className="p-2">{text}</div>
+    return <div className="flex py-2 text-gray-700 cursor-pointer hover:bg-gray-200 rounded max-w-48 pl-4 transition-all duration-200 ">
+        <div className="pr-2">{icon}</div>
+        <div >{text}</div>
 
     </div>
 }
